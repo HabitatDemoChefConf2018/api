fdaebc0a1f9384f280a68765c76b4a747310bba2
William Stewart <ws185067@ncr.com>
2018-05-15 10:31:08 -0400

    Remove facets resource. Add get sites call.

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,14 @@
-import { version } from '../../package.json';
 import { Router } from 'express';
-import facets from './facets';
+import config from '../config';
+import { version } from '../../package.json';
 
 export default ({ config, db }) => {
 	let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+	api.get('/sites', (req, res) => {
+		res.json(config.sites);
+	});
 
-	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
 		res.json({ version });
 	});
--- src/config.js
+++ src/config.js
@@ -3,6 +3,9 @@ export default {
 	bodyLimit: '100kb',
 	corsHeaders: ['Link'],
 	sites: [
-		{ id: '1', name: 'sites_001', uri: 'http://localhost:8081' }
-	]
+		{ id: '1', name: 'sites_001', uri: 'http://localhost:8090' }
+	],
+	apis: [
+		{ id: '1', name: 'api_001', uri: 'http://localhost:8082' }
+	],
 }
